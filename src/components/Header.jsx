fc403354e496e92a78d8a1827907f83463aeec79
JohnGuilding <jwguilding@gmail.com>
2022-03-22 21:01:12 +0000

    Add font awesome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 import './../styles/header.scss';
 
 const Header = () => {
+
+    const headerIcon = <FontAwesomeIcon icon={faBars} />
+
     return (
         <header className='header'>
             <div>
